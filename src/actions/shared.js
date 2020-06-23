d978e7e2430d53e86f5ae8d264afa3d2f5db64eb
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-23 11:04:54 +0530

    apply conne ct in app.js

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -5,7 +5,7 @@ import { setAuthedUser } from "../actions/authedUser";
 
 const AUTHED_ID = "tylermcginnis";
 
-export function handleInitailData() {
+export function handleInitialData() {
 	return (dispatch) => {
 		return getInitialData().then(({ users, tweets }) => {
 			dispatch(receiveUsers(users));
