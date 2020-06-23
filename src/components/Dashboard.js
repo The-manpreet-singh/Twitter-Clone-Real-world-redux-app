5dbd3f023b3ca8460b08ec66eb01040b41ac5997
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-23 11:44:28 +0530

    add tweets

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Tweet from './Tweet'
 
 class Dashboard extends Component {
 	render() {
@@ -9,7 +10,7 @@ class Dashboard extends Component {
 				<ul className="dashboard-list">
 					{this.props.tweetIds.map((id) => (
 						<li key={id}>
-							<div>TWEET ID: {id}</div>
+            <Tweet id={id}/>
 						</li>
 					))}
 				</ul>
