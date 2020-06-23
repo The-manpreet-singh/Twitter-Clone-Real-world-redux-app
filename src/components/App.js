966798ac5738c500b3bea66cb53c46e447603a92
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-23 12:21:46 +0530

    add Nav.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from 'react-redux-loading'
 import NewTweet from './NewTweet'
 import TweetPage from './TweetPage'
+import Nav from './Nav'
 
 class App extends Component {
 	componentDidMount() {
