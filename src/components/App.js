54b8e29a24d95921b65801969ba2aa1435e436bf
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-23 11:20:09 +0530

    import dashboard in index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { handleInitialData } from '../actions/shared'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(handleInitialData())
-  }
-  render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
-  }
+	componentDidMount() {
+		this.props.dispatch(handleInitialData());
+	}
+	render() {
+		return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
+	}
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null,
+	};
+}
+
+export default connect(mapStateToProps)(App);
