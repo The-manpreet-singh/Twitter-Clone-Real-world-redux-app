76f86d3e928d730294d642352744d7d7085777e6
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-23 12:18:23 +0530

    add TweetPage in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from 'react-redux-loading'
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 class App extends Component {
 	componentDidMount() {
@@ -12,7 +13,8 @@ class App extends Component {
 	render() {
     return <div>
        <LoadingBar />
-      {this.props.loading === true ? null : <NewTweet />}</div>;
+      {this.props.loading === true ? null 
+        : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>}</div>;
 	}
 }
 
