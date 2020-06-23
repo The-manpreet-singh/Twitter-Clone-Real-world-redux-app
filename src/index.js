990e41dfe4fc8985627e8b265586d9efec46867d
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-23 10:43:19 +0530

    ads provider in index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+ReactDOM.render(<App />, document.getElementById('root')) 
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+) 
